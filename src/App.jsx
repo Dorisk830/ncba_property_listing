2e4923b5914b505df5a16602c3445ae42ae609e4
DorisK830 <doris.kiprop@student.moringaschool.com>
2024-11-14 23:04:12 +0300

    Pages and Routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import {Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
 
-export default function Apple() {
-  return <h1 className='text-red-600' >App</h1>
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-out" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+
+  );
 }
