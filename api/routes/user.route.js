5567f3dd31985dabf2cb0111538a0d3249fbd73b
DorisK830 <doris.kiprop@student.moringaschool.com>
2024-11-16 11:41:48 +0300

    test api Route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,14 +1,3 @@
-// import express from 'express';
-
-// const router = express.Router();
-// router.get('/test', (req, res)=>{
-//     res.json({
-//         message:'Hello world'
-//     });
-// }
-// );
-
-// export default router;
 import express from 'express';
 import {test} from '../controllers/user.controller.js';
 
