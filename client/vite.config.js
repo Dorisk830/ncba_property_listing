416126cd678837d88f2d58b096fe73cae04b9a40
DorisK830 <doris.kiprop@student.moringaschool.com>
2024-11-20 15:03:56 +0300

    signup page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,11 +10,17 @@ export default defineConfig({
     },
     host: true,          // Enable network access (useful if testing on other devices)
     port: 5174,          // Use a specific port (adjust if needed)
-    strictPort: false,    // Fail if port is already in use
+    strictPort: false,   // Fail if port is already in use
     hmr: {
       protocol: 'ws',    // Use WebSocket for HMR
       host: 'localhost',
     },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        // changeOrigin: true,  // Change the origin of the host header to the target URL
+        secure: false,       // Disable SSL verification (useful for local development)
+      },
+    },
   },
 });
-
