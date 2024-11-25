438025285517ab44634a67219f22dfc959a6276b
DorisK830 <doris.kiprop@student.moringaschool.com>
2024-11-25 20:55:46 +0300

    ok

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,15 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import './index.css';
-import {persistor, store} from './redux/store.js';
-import {Provider} from 'react-redux';
+import { persistor, store } from './redux/store.js';
+import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-
-// Get the root element from your HTML file (usually `index.html`)
-const rootElement = document.getElementById('root');
-
-// Create a root using React 18's new API
-const root = ReactDOM.createRoot(rootElement);
-
-// Render your application
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate loading= {null} persistor={persistor}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
     </PersistGate>
   </Provider>
 );
